d44e54050e9832cc87798041db67c58f57405577
nightcode-dev <prog.nightcode@gmail.com>
2022-09-12 20:06:56 +0430

    init jwt

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -1,11 +1,24 @@
 import { Module } from '@nestjs/common';
+import { JwtModule } from '@nestjs/jwt';
+import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
 import { User } from './user.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [
+    TypeOrmModule.forFeature([User]),
+  PassportModule.register({
+    defaultStrategy:"jwt"
+  }),
+  JwtModule.register({
+    secret:'safePassword',
+    signOptions:{
+      expiresIn:3600
+    }
+  })
+],
   controllers: [AuthController],
   providers: [AuthService],
 })
