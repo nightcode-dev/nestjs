3c8a03adcad7c70ec8e84837c68a9188d611c823
nightcode-dev <prog.nightcode@gmail.com>
2022-09-17 17:27:51 +0430

    database relation

--- src/auth/user.entity.ts
+++ src/auth/user.entity.ts
@@ -1,4 +1,5 @@
-import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from 'typeorm';
+import {Tasks} from '../tasks/tasks.entity'
 
 @Entity()
 export class User {
@@ -12,4 +13,7 @@ export class User {
 
   @Column()
   password: string;
+
+  @OneToMany(type => Tasks,tasks => tasks.user,{eager:true})
+  tasks:Tasks[]
 }
